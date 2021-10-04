030edafb625fa4a1a0b209f5082c65b9bc97075c
Your Name <iankaveli0@gmail.com>
2021-10-04 17:56:08 +0300

    New Edits

---
+++ Public/js/datasetheader.js
@@ -0,0 +1,13 @@
+let Productclass= 'product-card'
+let  Imageclass="product-image"
+let Discountclass="discount-tag"
+let DiscountPercent = '50% off'
+let image ="img/card1.png" 
+let Imgclass="product-thumb"
+let BtnClass = "card-btn"
+let Infoclass="product-info"
+let h2class="product-brand"
+let Paraclass="product-short-des"
+let Priceclass="price"
+let ActualPriceClass="actual-price"
+                    
